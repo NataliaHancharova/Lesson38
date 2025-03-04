d653710f2682e80f2e1c9b64663447ea6691ce8e
NataliaHancharova <Natasha_san@mail.ru>
2025-03-05 00:04:28 +0300

    initial commit
    initial commit

---
+++ z2.py
@@ -0,0 +1,22 @@
+from aiohttp import web
+import aiohttp_jinja2
+import jinja2
+
+app = web.Application()
+
+aiohttp_jinja2.template('index.html')
+
+
+aiohttp_jinja2.setup(app, loader = jinja2.FailSistemLoader('/.templates'))
+
+async def index(request):
+    list_dict = [ {"title": "Первый пост",
+                    "content": "Содержимое первого поста"}, 
+                    {"title": "Второй пост", "content": "Содержимое второго поста"}, 
+                    {"title": "Третий пост", "content": "Содержимое третьего поста"} 
+                ]
+    return {'poats': list_dict}
+
+app.add_routes([web.get('/', index)])
+
+web.run_app(app, host='127.0.0.1', port=8080)
